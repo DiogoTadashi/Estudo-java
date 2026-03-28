0784d047a1eabe2efc0ee8d1ae49820222234b96
DiogoTadashi <diogotadashi22@gmail.com>
2026-03-28 13:22:04 -0300

    Arrays

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int [] idade = new int[3];
+        idade[0] = 21;
+        idade[1] = 15;
+        idade[2] = 24;
+
+        System.out.println(idade[0]);
+        System.out.println(idade[1]);
+        System.out.println(idade[2]);
+    }
+}
