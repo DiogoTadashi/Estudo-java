5caf38d3f3cb381f06a902a61c1db0c11789c61a
DiogoTadashi <diogotadashi22@gmail.com>
2026-03-30 11:02:38 -0300

    feat: add 1 class for OOP study

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -10,7 +10,30 @@ public class Calculadora {
         System.out.println(21 - 2);
     }
 
-    public void multiplicaDoisNumeros(int num1, int num2){
+    public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
+    }
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        System.out.println("não existe divisão por 2");
+        return 0;
+    }
+
+    public void  imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0){
+            System.out.println("não existe divisão por 2");
+            return;
+        }
+        System.out.println(num1/num2);
     }
 }
