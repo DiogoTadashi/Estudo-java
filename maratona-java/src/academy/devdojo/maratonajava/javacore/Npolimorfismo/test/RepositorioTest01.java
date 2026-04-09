4fc611b154d9f757302904ef824b248cdf2195d0
DiogoTadashi <diogotadashi22@gmail.com>
2026-04-09 17:35:45 -0300

    Feat(OOP-study): add class for test

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.Repositorio.Repositorio;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioArquivo;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioMemoria;
+
+public class RepositorioTest01 {
+    public static void main(String[] args) {
+        Repositorio repositorio = new RepositorioBancoDeDados();
+        Repositorio repositorio2 = new RepositorioArquivo();
+        Repositorio repositorio3 = new RepositorioMemoria();
+        repositorio.salvar();
+        repositorio2.salvar();
+        repositorio3.salvar();
+
+        
+    }
+}
