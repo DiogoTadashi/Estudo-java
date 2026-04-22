bd0001b137f938892b4ac2dbe0fd4ddcebd78bfe
DiogoTadashi <diogotadashi22@gmail.com>
2026-04-22 15:05:00 -0300

    feat: learning abt instant and localdatetime

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTimeTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.of(2022, Month.APRIL,3);
+        LocalTime time = LocalTime.of(22,12,03);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime.toLocalDate());
+        LocalDateTime ldt = date.atTime(time);
+        LocalDateTime ldtNow = time.atDate(LocalDate.now());
+        System.out.println(ldt);
+        System.out.println(ldtNow);
+
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(11,12,04);
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.AMPM_OF_DAY));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+    }
+}
